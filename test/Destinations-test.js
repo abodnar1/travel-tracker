447f56ff9f35eb5db38072f5aa8de3138eec0462
Amber Bodnar <amber.bodnar@gmail.com>
2022-06-09 17:02:37 -0400

    Change to ES6 syntax in Destinations-test.js file

---
+++ test/Destinations-test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import {
+  allDestinationsData
+} from './sample-data';
+
+describe('Destinations Test', () => {
+  let destinations;
+  // declare variables here using 'let'
+
+  beforeEach(() => {
+    // assign variables here
+    destinations = new Destinations(data)
+
+  });
+
+
+  it.skip('should be a function',() => {
+    expect(Destinations).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Sleep', () => {
+    expect(destinations).to.be.instanceOf(Destinations);
+  });
+
+});
