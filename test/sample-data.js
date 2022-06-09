6836776b9288d18715170b27e422266550ea3353
Amber Bodnar <amber.bodnar@gmail.com>
2022-06-09 18:18:19 -0400

    Decrease number of data points in sample data file

--- test/sample-data.js
+++ test/sample-data.js
@@ -23,31 +23,6 @@ const allTravelersData = [
     id: 5,
     name: "Tiffy Grout",
     travelerType: "thrill-seeker"
-  },
-  {
-    id: 6,
-    name: "Laverna Flawith",
-    travelerType: "shopper"
-  },
-  {
-    id: 7,
-    name: "Emmet Sandham",
-    travelerType: "relaxer"
-  },
-  {
-    id: 8,
-    name: "Carlin O'Reilly",
-    travelerType: "history buff"
-  },
-  {
-    id: 9,
-    name: "Natalee Deegin",
-    travelerType: "relaxer"
-  },
-  {
-    id: 10,
-    name: "Rickie Jodlowski",
-    travelerType: "relaxer"
   }
 ];
 
@@ -102,56 +77,6 @@ const allTripsData = [
     status: "approved",
     suggestedActivities: [ ]
   },
-  {
-    id: 6,
-    userID: 29,
-    destinationID: 35,
-    travelers: 3,
-    date: "2022/06/29",
-    duration: 9,
-    status: "approved",
-    suggestedActivities: [ ]
-  },
-  {
-    id: 7,
-    userID: 37,
-    destinationID: 17,
-    travelers: 5,
-    date: "2022/5/28",
-    duration: 20,
-    status: "approved",
-    suggestedActivities: [ ]
-  },
-  {
-    id: 8,
-    userID: 36,
-    destinationID: 39,
-    travelers: 6,
-    date: "2022/02/07",
-    duration: 4,
-    status: "approved",
-    suggestedActivities: [ ]
-  },
-  {
-    id: 9,
-    userID: 24,
-    destinationID: 19,
-    travelers: 5,
-    date: "2022/12/19",
-    duration: 19,
-    status: "approved",
-    suggestedActivities: [ ]
-  },
-  {
-    id: 10,
-    userID: 9,
-    destinationID: 50,
-    travelers: 6,
-    date: "2022/07/23",
-    duration: 17,
-    status: "approved",
-    suggestedActivities: [ ]
-  }
 ];
 
 const allDestinationsData = [
@@ -194,45 +119,6 @@ const allDestinationsData = [
     estimatedFlightCostPerPerson: 650,
     image: "https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     alt: "city with clear skys and a road in the day time"
-  },
-  {
-    id: 6,
-    destination: "Jakarta, Indonesia",
-    estimatedLodgingCostPerDay: 70,
-    estimatedFlightCostPerPerson: 890,
-    image: "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    alt: "lit up city at night"
-  },
-  {
-    id: 7,
-    destination: "Paris, France",
-    estimatedLodgingCostPerDay: 100,
-    estimatedFlightCostPerPerson: 395,
-    image: "https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
-    alt: "city during the day time with eiffel tower"
-  },
-  {
-    id: 8,
-    destination: "Tokyo, Japan",
-    estimatedLodgingCostPerDay: 125,
-    estimatedFlightCostPerPerson: 1000,
-    image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1971&q=80",
-    alt: "city with people walking in crosswalk and brightly lit shops at night"
-  },
-  {
-    id: 9,
-    destination: "Amsterdam, Netherlands",
-    estimatedLodgingCostPerDay: 100,
-    estimatedFlightCostPerPerson: 950,
-    image: "https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    alt: "canal with boats and trees and buildings along the side"
-  },
-  {
-    id: 10,
-    destination: "Toronto, Canada",
-    estimatedLodgingCostPerDay: 90,
-    estimatedFlightCostPerPerson: 450,
-    image: "https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80"
   }
 ];
 
