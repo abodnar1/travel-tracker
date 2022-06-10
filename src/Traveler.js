adcd7be6551038bfdc98dd31838f41c754fe09cb
Amber Bodnar <amber.bodnar@gmail.com>
2022-06-10 16:44:57 -0400

    Restructure trip test and class

--- src/Traveler.js
+++ src/Traveler.js
@@ -10,11 +10,6 @@ class Traveler {
     return this.myTrips = tripsData.filter(trip => this.id === trip.userID);
   };
 
-  // need a function for past trips?
-  // need a function for present trips?
-  // need a function for future trips?
-  // need a function for pending trips?
-
 };
 
 export default Traveler;
---
+++ src/Trip.js
@@ -0,0 +1,14 @@
+class Trip {
+  constructor(trip) {
+    this.id = trip.id;
+    this.userID = trip.userID;
+    this.destinationID = trip.destinationID;
+    this.travelers = trip.travelers;
+    this.date = trip.date;
+    this.duration = trip.duration;
+    this.status = trip.status;
+    this.suggestedActivities = trip.suggestedActivities;
+  };
+};
+
+export default Trip;
